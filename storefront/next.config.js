e5715afa771a788049dd05ba783a0b25d9c1a353
Thanh Nguyen <thanhnguyen.rynt@gmail.com>
2025-03-15 22:21:04 +0700

    ..

--- storefront/next.config.js
+++ storefront/next.config.js
@@ -1,15 +1,26 @@
 /** @type {import('next').NextConfig} */
 const config = {
 	images: {
-		remotePatterns: [
-			{
-				hostname: "*",
-			},
-		],
-	},
+        domains: ['localhost', 'host.docker.internal','saleor-api.fly.dev'],
+        remotePatterns: [
+          {
+            protocol: 'http',
+            hostname: '**',
+          },
+        ],
+        unoptimized: true, // Thêm dòng này để tắt tối ưu hình ảnh
+    },
 	experimental: {
 		typedRoutes: false,
 	},
+	async rewrites() {
+		return [
+		  {
+			source: '/thumbnail/:path*',
+			destination: 'https://saleor-api.fly.dev/thumbnail/:path*',
+		  },
+		];
+	  },
 	// used in the Dockerfile
 	output:
 		process.env.NEXT_OUTPUT === "standalone"
